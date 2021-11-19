a36a7867c7a3b5d402d40d3902e2fa48a806acb4
Hugo KELHETTER <hkelhetter@unistra.fr>
2021-11-19 14:21:13 +0100

    tp

--- back/models/index.js
+++ back/models/index.js
@@ -1,21 +1,20 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 const db = {};
 
-let sequelize = new Sequelize(process.env.BDD_URL)
+const sequelize = new Sequelize(process.env.BDD_URL);
 
 fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
-      file.indexOf('.') !== 0 &&
+  .filter((file) => (
+    file.indexOf(".") !== 0 &&
       file !== basename &&
-      file.slice(-3) === '.js' &&
-      file != 'config.js'
-    );
-  })
+      file.slice(-3) === ".js" &&
+      file != "config.js"
+  ))
   .forEach((file) => {
     const model = require(path.join(__dirname, file))(
       sequelize,
--- front/src/api/questions.js
+++ front/src/api/questions.js
@@ -5,30 +5,38 @@ const questions = {
     return fetch(`${host}/questions`, {
       method: "POST",
       headers: {
-        "content-type": "application/json",
+        "content-type": "application/json"
       },
       body: JSON.stringify(body)
-    }).then(checkStatus).then(res => res.json())
+    })
+      .then(checkStatus)
+      .then((res) => res.json());
   },
   getAll: () => {
     return fetch(`${host}/questions`, {
       method: "GET"
-    }).then(checkStatus).then(res => res.json())
+    })
+      .then(checkStatus)
+      .then((res) => res.json());
   },
   getOne: (id) => {
     return fetch(`${host}/questions/${id}`, {
       method: "GET"
-    }).then(checkStatus).then(res => res.json())
+    })
+      .then(checkStatus)
+      .then((res) => res.json());
   },
-  answer: ({id, body}) => {
+  answer: ({ id, body }) => {
     return fetch(`${host}/questions/${id}/answer`, {
       method: "POST",
       headers: {
         "content-type": "application/json"
       },
       body: JSON.stringify(body)
-    }).then(checkStatus).then(res => res.json())
+    })
+      .then(checkStatus)
+      .then((res) => res.json());
   }
-}
+};
 
 export default questions;
